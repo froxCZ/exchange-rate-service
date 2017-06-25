79f6e865191f91ba5dfd7ef1a9b7cf35e34c7b9e
Vojtech.Udrzal <udrzalv@gmail.com>
2017-06-25 20:58:47 +0200

    removed optional in repository class

--- src/main/java/com/avrios/sample/exchange/repository/ExchangeRepository.java
+++ src/main/java/com/avrios/sample/exchange/repository/ExchangeRepository.java
@@ -2,7 +2,6 @@ package com.avrios.sample.exchange.repository;
 
 import java.util.Date;
 import java.util.Map;
-import java.util.Optional;
 
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
@@ -27,15 +26,19 @@ public class ExchangeRepository {
     @NotNull
     public ExchangeRate getExchangeRateOnDay(@NotNull String currency, @Nullable Date date) {
         //TODO: consider exception if data were not updated for certain time especially when date is null (=today).
-        DayExchangeRates dayExchangeRates = Optional
-                .ofNullable(this.dayExchangeRates)
-                .orElseThrow(() -> new ExchangeRateNotFound("Exchange rates are not available yet. Try again later."));
-        date = Optional.ofNullable(date).orElse(latestDate);
+        if (dayExchangeRates == null) {
+            throw new ExchangeRateNotFound("Exchange rates are not available yet. Try again later.");
+        }
+        date = date != null ? date : latestDate;
         currency = currency.toUpperCase();
-        Map<String, Double> rates = Optional.ofNullable(dayExchangeRates.get(date))
-                .orElseThrow(() -> new ExchangeRateNotFound("No data available for specified date."));
-        Double rate = Optional.ofNullable(rates.get(currency))
-                .orElseThrow(() -> new ExchangeRateNotFound("No exchange rate for specified currency."));
+        Map<String, Double> rates = dayExchangeRates.get(date);
+        if (rates == null) {
+            throw new ExchangeRateNotFound("No data available for specified date.");
+        }
+        Double rate = rates.get(currency);
+        if (rate == null) {
+            throw new ExchangeRateNotFound("No exchange rate for specified currency.");
+        }
         return new ExchangeRate(date, currency, rate);
     }
 
